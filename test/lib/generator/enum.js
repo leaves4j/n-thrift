1b8f5605a6d1c4602f1494a7a1734408e9f77d3c
jiangq <jiangq.leaves@gmail.com>
2017-11-05 23:04:35 +0800

    add enum generator

---
+++ test/lib/generator/enum.js
@@ -0,0 +1,41 @@
+const test = require('ava');
+const enumGenerator = require('../../../lib/generator/enum');
+
+test('enumGenerator', (t) => {
+  const ast = [
+    {
+      type: 'enumField',
+      identifier: 'ONE',
+      value: null
+    },
+    {
+      type: 'enumField',
+      identifier: 'TWO',
+      value: '2'
+    },
+    {
+      type: 'enumField',
+      identifier: 'THREE',
+      value: null
+    },
+    {
+      type: 'enumField',
+      identifier: 'FIVE',
+      value: '5'
+    },
+    {
+      type: 'enumField',
+      identifier: 'SIX',
+      value: null
+    },
+    {
+      type: 'enumField',
+      identifier: 'EIGHT',
+      value: '8'
+    }
+  ];
+
+  t.snapshot(enumGenerator('Numberz', ast));
+  t.snapshot(enumGenerator('Numberz', ast, { moduleFormat: 'esm' }));
+});
+
