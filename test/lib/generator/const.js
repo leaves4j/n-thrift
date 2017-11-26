db04b1ef47945f0b56989866be682537c58f8626
jiangq <jiangq.leaves@gmail.com>
2017-11-26 20:45:09 +0800

    add const

---
+++ test/lib/generator/const.js
@@ -0,0 +1,56 @@
+
+const test = require('ava');
+const _ = require('lodash');
+const constGenerator = require('../../../lib/generator/const');
+
+test('enumGenerator', (t) => {
+  const constMap = {
+    fieldType: {
+      keyFieldType: 'i32',
+      type: 'map',
+      valueFieldType: 'string'
+    },
+    identifier: 'myNumberza2',
+    type: 'const',
+    value: [
+      {
+        key: '1',
+        value: '\'123\''
+      },
+      {
+        key: '2',
+        value: '\'123\''
+      }
+    ]
+  };
+  t.snapshot(constGenerator(constMap.identifier, constMap.fieldType, constMap.value));
+  const constString = {
+    fieldType: 'i32',
+    identifier: 'cNumber',
+    type: 'const',
+    value: '123'
+  };
+  t.snapshot(constGenerator(constString.identifier, constString.fieldType, constString.value));
+  const constStruct = {
+    fieldType: 'Xtruct',
+    identifier: 'myStruct',
+    type: 'const',
+    value: [
+      {
+        key: '\'string_thing\'',
+        value: '\'123\''
+      },
+      {
+        key: '\'byte_thing\'',
+        value: '123'
+      }
+    ]
+  };
+  t.snapshot(constGenerator(constStruct.identifier, constStruct.fieldType, constStruct.value));
+  const options = {
+    moduleFormat: 'esm',
+    fieldFormatter: _.camelCase
+  };
+  t.snapshot(constGenerator(constStruct.identifier, constStruct.fieldType, constStruct.value, options));
+});
+
